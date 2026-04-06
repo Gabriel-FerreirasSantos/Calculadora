f65ea53fc9776979b44bc54b3bff911063816a3b
Gabriel Ferreiras <83989808+Gabriel-FerreirasSantos@users.noreply.github.com>
2026-04-06 00:54:51 -0300

    Iniciando a Estrutura do software
    feat: implementa loop principal, menu de opções e controle de fluxo

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 #Começo da calculadora
 
+
+#Funções
+    #Letreiro: serve para deixar o programa estético para o usuario
+def letreiro():
+    print(25*"=")
+    print("Programa principal".center(23, ' '))
+    print(25*"=")
+    print("[1] Calculadora")
+    print("[2] Desafios matematicos")
+    print("[3] Sair")
+    print(25*"=")
+
+
+
+#Estrutura de repetição
+while True:  #apenas um while True para manter o programa funcionando para testar
+
+    letreiro() #letreiro
+    resposta = str(input("Qual você deseja :")).strip() # pegando a resposta do usuario
+
+    #Estrutura de Condição:
+    if resposta == "1":
+        print("Número 1")
+
+    elif resposta == "2":
+        print("Número 2")
+
+    elif resposta == "3":
+        print("Número 3")
+
+    else:
+        print(f"{resposta} não existe.")
+
+    break #quebrar a estrutura de repetição
